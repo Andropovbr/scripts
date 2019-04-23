b471f6e3726c8676ced05782c142ab40c49ff204
André Luiz Fernandes dos Santos <andresantosbr@gmail.com>
2019-04-23 18:02:33 -0300

    Added comments

--- clean_acl.py
+++ clean_acl.py
@@ -7,28 +7,28 @@
 
 import tldextract
 
-#lê arquivo com as ACLs
-file_acl = open("ca_saobentodosul.txt", "r")
+#Read ACLs from file
+file_acl = open("file.txt", "r")
 list_subdomains = file_acl.readlines()
 int_subdomains_count = len(list_subdomains)
 
 list_domains = []
 i = 0
 
-#mantém apenas os domíniosde uma URL ou subdomínio
+#Extract domain name from subdomains
 for item in list_subdomains:
 	list_domains.append(tldextract.extract(item.rstrip()).registered_domain)
 	i += 1
 	print("Processed "+ str(i) + " entries of "+ str(int_subdomains_count), end='\r')
 
-#remove redundâncias
+#Remove redundant domains
 list_domains = list(dict.fromkeys(list_domains))
 domain_count = len(list_domains)
 
 print("\n"+"Deleted entries: "+str(int_subdomains_count - domain_count))
 print("Domain count: "+str(domain_count))
 
-#escreve num arquivo a nova lista de ACLs
+#Write result in a file
 with open('acl_new.txt', mode='wt', encoding='utf-8') as myfile:
     myfile.write('\n'.join(list_domains))
 
